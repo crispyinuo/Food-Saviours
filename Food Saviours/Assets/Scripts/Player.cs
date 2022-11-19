0adbc45c798186e4a161e503dd6148769fcdd405
Zoe Zhou <zoez@usc.edu>
2022-11-19 03:59:49 -0800

    add pick up

--- Food Saviours/Assets/Scripts/Player.cs
+++ Food Saviours/Assets/Scripts/Player.cs
@@ -5,11 +5,12 @@ public class Player : MonoBehaviour {
 
 		private Animator anim;
 		private CharacterController controller;
-
+		public float m_foodRange = 0f;
 		public float speed = 600.0f;
 		public float turnSpeed = 400.0f;
 		private Vector3 moveDirection = Vector3.zero;
 		public float gravity = 20.0f;
+
 
 		void Start () {
 			controller = GetComponent <CharacterController>();
@@ -17,12 +18,14 @@ public class Player : MonoBehaviour {
 		}
 
 		void Update (){
-			if (Input.GetKey ("w")) {
+			if (Input.GetKey ("w")||Input.GetKey ("s")) {
 				anim.SetInteger ("AnimationPar", 1);
 			}  else {
 				anim.SetInteger ("AnimationPar", 0);
 			}
-
+			if(Input.GetKey("space")){
+				GetFood();
+			}
 			if(controller.isGrounded){
 				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
 			}
@@ -31,5 +34,21 @@ public class Player : MonoBehaviour {
 			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 			controller.Move(moveDirection * Time.deltaTime);
 			moveDirection.y -= gravity * Time.deltaTime;
+		}
+
+		private void GetFood(){
+			Collider[] hitColliders = Physics.OverlapSphere(transform.position, m_foodRange);
+            for (int i = 0; i < hitColliders.Length; i++){
+                GameObject hitCollider = hitColliders[i].gameObject;
+                if (hitCollider.GetComponent<Food>()!=null)
+                {
+					anim.SetTrigger("GetFood");
+					hitCollider.GetComponent<Food>().Collect();
+					Debug.Log("Collect the Food!");	
+					if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime>0.5f) 
+					{				
+					}
+                }
+            }
 		}
 }
